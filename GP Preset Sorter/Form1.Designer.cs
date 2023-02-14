06a11a47a687b030dd52cbf6d0d0628a2650d48b
Carlos Garcia <carlos@ciyi.es>
2023-02-14 12:55:11 +0100

    Initial commit

---
+++ GP Preset Sorter/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace GP_Preset_Sorter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.presets_listBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadPresetsFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportPresetsFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.filePath_label1 = new System.Windows.Forms.Label();
+            this.filePath_label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // presets_listBox
+            // 
+            this.presets_listBox.FormattingEnabled = true;
+            this.presets_listBox.Location = new System.Drawing.Point(44, 101);
+            this.presets_listBox.Name = "presets_listBox";
+            this.presets_listBox.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.presets_listBox.Size = new System.Drawing.Size(381, 277);
+            this.presets_listBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "List";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(557, 24);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadPresetsFileToolStripMenuItem,
+            this.exportPresetsFileToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadPresetsFileToolStripMenuItem
+            // 
+            this.loadPresetsFileToolStripMenuItem.Name = "loadPresetsFileToolStripMenuItem";
+            this.loadPresetsFileToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
+            this.loadPresetsFileToolStripMenuItem.Text = "Import presets file";
+            this.loadPresetsFileToolStripMenuItem.Click += new System.EventHandler(this.loadPresetsFileToolStripMenuItem_Click);
+            // 
+            // exportPresetsFileToolStripMenuItem
+            // 
+            this.exportPresetsFileToolStripMenuItem.Enabled = false;
+            this.exportPresetsFileToolStripMenuItem.Name = "exportPresetsFileToolStripMenuItem";
+            this.exportPresetsFileToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
+            this.exportPresetsFileToolStripMenuItem.Text = "Export presets file";
+            this.exportPresetsFileToolStripMenuItem.Click += new System.EventHandler(this.exportPresetsFileToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem1});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // aboutToolStripMenuItem1
+            // 
+            this.aboutToolStripMenuItem1.Name = "aboutToolStripMenuItem1";
+            this.aboutToolStripMenuItem1.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem1.Text = "About";
+            this.aboutToolStripMenuItem1.Click += new System.EventHandler(this.aboutToolStripMenuItem1_Click);
+            // 
+            // filePath_label1
+            // 
+            this.filePath_label1.AutoSize = true;
+            this.filePath_label1.Location = new System.Drawing.Point(42, 43);
+            this.filePath_label1.Name = "filePath_label1";
+            this.filePath_label1.Size = new System.Drawing.Size(64, 13);
+            this.filePath_label1.TabIndex = 11;
+            this.filePath_label1.Text = "Presets file: ";
+            this.filePath_label1.Visible = false;
+            // 
+            // filePath_label2
+            // 
+            this.filePath_label2.AutoSize = true;
+            this.filePath_label2.Location = new System.Drawing.Point(103, 43);
+            this.filePath_label2.Name = "filePath_label2";
+            this.filePath_label2.Size = new System.Drawing.Size(35, 13);
+            this.filePath_label2.TabIndex = 12;
+            this.filePath_label2.Text = "label3";
+            this.filePath_label2.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Image = global::GP_Preset_Sorter.Properties.Resources.down_arrow;
+            this.button2.Location = new System.Drawing.Point(450, 131);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 14;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Image = global::GP_Preset_Sorter.Properties.Resources.up_arrow1;
+            this.button1.Location = new System.Drawing.Point(450, 101);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 13;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(557, 408);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.filePath_label2);
+            this.Controls.Add(this.filePath_label1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.presets_listBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Valeton GP-100 Preset sorter";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox presets_listBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadPresetsFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exportPresetsFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Label filePath_label1;
+        private System.Windows.Forms.Label filePath_label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem1;
+    }
+}
+
---
+++ GP Preset Sorter/Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace GP_Preset_Sorter
+{
+    public partial class Form1 : Form
+    {
+        //Max numbers of presets that will be in the prst file
+        public const int MaxPresets = 99;
+        public XmlDocument rootXmlDocument;
+
+        //List used for storing the actual preset XML
+        public List<XmlNode> presetsListXml = new List<XmlNode>();
+        //List used for showing the preset names in the ListView
+        public List<String> presetsListItems = new List<String>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void loadPresetsFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string filePath = LoadPresetsFile();
+            XmlNodeList xmlPresets = ReadXMLFileUsingXMLDocument(filePath);
+            if (xmlPresets != null)
+            {
+                filePath_label2.Text = filePath;
+                filePath_label1.Visible = true;
+                filePath_label2.Visible = true;
+
+                LoadXMLIntoList(xmlPresets);
+            }
+        }
+
+        internal XmlNodeList ReadXMLFileUsingXMLDocument(string filePath)
+        {
+            if (filePath != "")
+            {
+                try
+                {
+                    rootXmlDocument = new XmlDocument();
+                    rootXmlDocument.Load(filePath);
+                    XmlNodeList xmlNodeList = rootXmlDocument.DocumentElement.SelectNodes("/GP-100/presets");
+
+                    return xmlNodeList;
+                }
+                catch (System.Xml.XmlException e)
+                {
+                    MessageBox.Show("There was a problem with the structure of the file. Make sure it's a compatible Valeton GP-100 export file. \n\nError: " + e.Message);
+                    return null;
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("There was a problem opening the file. Make sure it's a compatible Valeton GP-100 export file. \n\nError: " + e.Message);
+                    return null;
+                }
+                
+            }
+
+            return null;
+   
+        }
+
+        private void LoadXMLIntoList (XmlNodeList xmlPresets)
+        {
+            presets_listBox.DataSource = null;
+            presets_listBox.Items.Clear();
+            presetsListItems.Clear();
+            presetsListXml.Clear();
+
+            for (int i = 0; i < xmlPresets.Count; i++)
+            {
+                //To show the whole XML preset
+                //MessageBox.Show(xmlPresets[i].OuterXml);
+                string presetNumber;
+                if (i < 9) { presetNumber = "0" + (i + 1); }
+                else { presetNumber = (i+1).ToString(); }
+                string presetItemTitle = "P" + presetNumber + ". " + xmlPresets[i].Attributes.GetNamedItem("ppName").InnerText;
+                presetsListItems.Add(presetItemTitle);
+                presetsListXml.Add(xmlPresets[i]);
+            }
+
+            presets_listBox.DataSource = presetsListItems;
+
+            exportPresetsFileToolStripMenuItem.Enabled = true;
+        }
+
+        private string LoadPresetsFile()
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Title = "Browse Valeton GP Presets Files";
+            openFileDialog1.DefaultExt = "prst";
+            openFileDialog1.Filter = "prst files (*.prst)|*.prst|All files (*.*)|*.*";
+            openFileDialog1.ShowDialog();
+
+            return openFileDialog1.FileName;
+
+        }
+
+        private void exportPresetsFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog ExportPresetsFileDialog = new SaveFileDialog();
+            ExportPresetsFileDialog.DefaultExt = "prst";
+            ExportPresetsFileDialog.Filter = "prst files (*.prst)|*.prst";
+            ExportPresetsFileDialog.ShowDialog();
+            String filePath = ExportPresetsFileDialog.FileName;
+
+            if(filePath != "")
+            {
+                XmlNodeList xmlNodeList = rootXmlDocument.DocumentElement.SelectNodes("/GP-100/presets");
+                foreach (XmlNode xmlPreset in xmlNodeList)
+                {
+                    rootXmlDocument.DocumentElement.SelectSingleNode("/GP-100").RemoveChild(xmlPreset);
+                }
+
+                //Add the presets in the new order
+                for (int i = 0; i < presetsListXml.Count; i++)
+                {
+                    //First, update the ppID XML attribute
+                    presetsListXml[i].Attributes["ppID"].Value = i.ToString();
+                    rootXmlDocument.DocumentElement.SelectSingleNode("/GP-100").AppendChild(presetsListXml[i]);
+                }
+
+                rootXmlDocument.Save(filePath);
+            }
+
+        }
+
+        private void moveItemsUp (ListBox.SelectedIndexCollection selectedIndices)
+        {
+
+            List<int> indices = new List<int>();
+            for (int i = 0; i < selectedIndices.Count; i++)
+            {
+                indices.Add(selectedIndices[i]);
+            }
+            //If we're trying to move further the limit (preset number 0)
+            if (selectedIndices[0] == 0)
+            {
+                return;
+            }
+
+            int index;
+
+            for (int i = 0; i < selectedIndices.Count; i++)
+            {
+                index = selectedIndices[i];
+                
+                //Update the list in the listbox
+                string presetName = presetsListItems[index];
+                presetsListItems.RemoveAt(index);
+                presetsListItems.Insert(index - 1, presetName);
+                
+                //Update the presets XML list
+                XmlNode presetXml = presetsListXml[index];
+                presetsListXml.RemoveAt(index);
+                presetsListXml.Insert(index - 1, presetXml);
+                
+            }
+
+            //Update the new selected items in the listbox
+            
+            presets_listBox.DataSource = null;
+            presets_listBox.DataSource = presetsListItems;
+            presets_listBox.ClearSelected();
+            for (int i = 0; i < indices.Count; i++)
+            {
+                presets_listBox.SetSelected(indices[i] - 1, true);
+            }
+        }
+
+        private void moveItemsDown(ListBox.SelectedIndexCollection selectedIndices)
+        {
+            List<int> indices = new List<int>();
+            for (int i = 0; i < selectedIndices.Count; i++)
+            {
+                indices.Add(selectedIndices[i]);
+            }
+            //If we're trying to move further the limit (preset number 99)
+            if (selectedIndices[(selectedIndices.Count - 1)] == (MaxPresets - 1))
+            {
+                return;
+            }
+
+            int index;
+            //MessageBox.Show("Total selected is " + selectedIndices.Count);
+
+            for (int i = (selectedIndices.Count - 1); i >=0 ; i--)
+            {
+                //MessageBox.Show("i=" + i);
+                index = selectedIndices[i];
+                //MessageBox.Show("Moving item at index " + index);
+                //Update the list in the listbox
+                string presetName = presetsListItems[index];
+                presetsListItems.RemoveAt(index);
+                presetsListItems.Insert(index + 1, presetName);
+
+                //Update the presets XML list
+                XmlNode presetXml = presetsListXml[index];
+                presetsListXml.RemoveAt(index);
+                presetsListXml.Insert(index + 1, presetXml);
+            }
+
+            //Update the new selected items in the listbox
+            
+            presets_listBox.DataSource = null;
+            presets_listBox.DataSource = presetsListItems;
+            presets_listBox.ClearSelected();
+            //MessageBox.Show("now we need to still update " + indices.Count + " selected items");
+            for (int i = 0; i < indices.Count; i++)
+            {
+                //MessageBox.Show("updating selected item with index " + (indices[i]+1));
+                presets_listBox.SetSelected(indices[i] + 1, true);
+            }
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            moveItemsUp(presets_listBox.SelectedIndices);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            moveItemsDown(presets_listBox.SelectedIndices);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void aboutToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            AboutBox1 about = new AboutBox1();
+
+            about.Show();
+
+        }
+    }
+}
